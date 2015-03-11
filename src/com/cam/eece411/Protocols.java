fcc76f7eebf99a374780b8c85ee9a5c925fbdcb1
cam <c.szarapka@alumni.ubc.ca>
2015-03-11 14:56:37 -0700

    Implemented KVStore, started Protocols

---
+++ src/com/cam/eece411/Protocols.java
@@ -0,0 +1,26 @@
+package com.cam.eece411;
+
+/**
+ * The constants used for commands at the app-layer and 
+ * the node-layer of our system, and response codes.
+ * @author cam
+ *
+ */
+public class Protocols {
+	// Commands that will be sent from a user of the DHT-KVStore
+	public static final byte APP_CMD_PUT		= 1;
+	public static final byte APP_CMD_GET		= 2;
+	public static final byte APP_CMD_REMOVE		= 3;
+	public static final byte APP_CMD_SHUTDOWN	= 4;
+	
+	// Commands that will be sent from a node in the DHT
+	
+	
+	// Response codes sent to the user of the DHT-KVStore
+	public static final byte CODE_SUCCESS			= 0;
+	public static final byte CODE_KEY_DNE			= 1;
+	public static final byte CODE_OUT_OF_SPACE		= 2;
+	public static final byte CODE_SYSTEM_OVERLOAD	= 3;
+	public static final byte CODE_INTERNAL_FAILURE	= 4;
+	public static final byte CODE_UNKNOWN_COMMAND	= 5;
+}
