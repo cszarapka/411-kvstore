2e4b328976fcdf3a17bc1f7b9ae376d42f948c06
g17-sb <redhot_bouthot@hotmail.com>
2015-03-31 15:27:01 -0700

    making testing easier

---
+++ Testing/MultiWindowTesting.php
@@ -0,0 +1,12 @@
+<?php
+
+shell_exec("scp kvStore.jar testingNodes.txt ubc_eece411_5@plab3.eece.ksu.edu:; scp kvStore.jar testingNodes.txt ubc_eece411_5@pl2.cs.yale.edu:; scp kvStore.jar testingNodes.txt ubc_eece411_5@pl1.pku.edu.cn:; scp kvStore.jar testingNodes.txt ubc_eece411_5@pl2.6test.edu.cn:; scp kvStore.jar testingNodes.txt ubc_eece411_5@planetlab2.cs.ubc.ca:; scp testingClient.jar ubc_eece411_5@planet-lab2.cs.ucr.edu:");
+
+shell_exec("sh term.sh \"printf '\\e[8;18;95t';printf '\\e[3;0;0t';ssh -i ~/.ssh/id_rsa ubc_eece411_5@planet-lab2.cs.ucr.edu -t 'killall -9 java; java -jar testingClient.jar; bash'\"");
+shell_exec("sh term.sh \"printf '\\e[8;18;95t';printf '\\e[3;0;280t';ssh -i ~/.ssh/id_rsa ubc_eece411_5@plab3.eece.ksu.edu -t 'killall -9 java; java -jar kvStore.jar; bash'\"");
+shell_exec("sh term.sh \"printf '\\e[8;18;95t';printf '\\e[3;0;565t';ssh -i ~/.ssh/id_rsa ubc_eece411_5@pl2.cs.yale.edu -t 'killall -9 java; java -jar kvStore.jar; bash'\"");
+shell_exec("sh term.sh \"printf '\\e[8;18;95t';printf '\\e[3;685;0t';ssh -i ~/.ssh/id_rsa ubc_eece411_5@pl1.pku.edu.cn -t 'killall -9 java; java -jar kvStore.jar; bash'\"");
+shell_exec("sh term.sh \"printf '\\e[8;18;95t';printf '\\e[3;685;280t';ssh -i ~/.ssh/id_rsa ubc_eece411_5@pl2.6test.edu.cn -t 'killall -9 java; java -jar kvStore.jar; bash'\"");
+shell_exec("sh term.sh \"printf '\\e[8;18;95t';printf '\\e[3;685;565t';ssh -i ~/.ssh/id_rsa ubc_eece411_5@planetlab2.cs.ubc.ca -t 'killall -9 java; java -jar kvStore.jar; bash'\"");
+
+?>
---
+++ Testing/Readme.txt
@@ -0,0 +1,3 @@
+1. Place term.sh, MultiWindowTesting.php, kvStore.jar, testingClient.jar, and testingNodes.txt all in the same folder
+
+2. cd to folder and run "php MultiWindowTesting.php"
