872ae2d1073e1fd6085f0a4b5d04af3de02d95b6
g17-sb <redhot_bouthot@hotmail.com>
2015-04-05 15:29:34 -0700

    client update

--- Testing/MultiWindowTesting.php
+++ Testing/MultiWindowTesting.php
@@ -1,6 +1,8 @@
 <?php
 
-shell_exec("scp kvStore.jar testingNodes.txt ubc_eece411_5@plab3.eece.ksu.edu:; scp kvStore.jar testingNodes.txt ubc_eece411_5@pl2.cs.yale.edu:; scp kvStore.jar testingNodes.txt ubc_eece411_5@pl1.pku.edu.cn:; scp kvStore.jar testingNodes.txt ubc_eece411_5@pl2.6test.edu.cn:; scp kvStore.jar testingNodes.txt ubc_eece411_5@planetlab2.cs.ubc.ca:; scp testingClient.jar ubc_eece411_5@planet-lab2.cs.ucr.edu:");
+if(sizeof($argv) == 1) {
+    shell_exec("scp kvStore.jar testingNodes.txt ubc_eece411_5@plab3.eece.ksu.edu:; scp kvStore.jar testingNodes.txt ubc_eece411_5@pl2.cs.yale.edu:; scp kvStore.jar testingNodes.txt ubc_eece411_5@pl1.pku.edu.cn:; scp kvStore.jar testingNodes.txt ubc_eece411_5@pl2.6test.edu.cn:; scp kvStore.jar testingNodes.txt ubc_eece411_5@planetlab2.cs.ubc.ca:; scp testingClient.jar ubc_eece411_5@planet-lab2.cs.ucr.edu:");
+}
 
 shell_exec("sh term.sh \"printf '\\e[8;18;95t';printf '\\e[3;0;0t';ssh -i ~/.ssh/id_rsa ubc_eece411_5@planet-lab2.cs.ucr.edu -t 'killall -9 java; sleep 10; java -jar testingClient.jar; bash'\"");
 shell_exec("sh term.sh \"printf '\\e[8;18;95t';printf '\\e[3;0;280t';ssh -i ~/.ssh/id_rsa ubc_eece411_5@plab3.eece.ksu.edu -t 'killall -9 java; java -jar kvStore.jar; bash'\"");
--- TestingClient/src/com/cam/test/eece411/TestingClient.java
+++ TestingClient/src/com/cam/test/eece411/TestingClient.java
@@ -50,30 +50,46 @@ public class TestingClient {
 
 		// Build some test data
 		byte[] key = Helper.generateRandomByteArray(32);
+		byte[] key1 = Helper.generateRandomByteArray(32);
+		byte[] key2 = Helper.generateRandomByteArray(32);
+		byte[] key3 = Helper.generateRandomByteArray(32);
 		byte[] value = Helper.generateRandomByteArray(50);
+		byte[][] keys = new byte[98][32];
 
-		boolean[] shouldWait = new boolean[10];
-		SendMessage[] messages = new SendMessage[10];
+		boolean[] shouldWait = new boolean[199];
+		SendMessage[] messages = new SendMessage[199];
 		messages[0] = new CreateTableRequest();
 		shouldWait[0] = false;
 		messages[1] = new GetRequest(key);			// should get KEY-DNE back
-		shouldWait[0] = true;
+		shouldWait[1] = true;
 		messages[2] = new RemoveRequest(key);		// should get KEY-DNE back
-		shouldWait[0] = true;
+		shouldWait[2] = true;
+		for(int i = 3; i < 101; i++) {
+			keys[i-3] = Helper.generateRandomByteArray(32);
+			messages[i] = new PutRequest(keys[i - 3],Helper.generateRandomByteArray(50));
+			shouldWait[i] = true;
+		}
+		for(int i = 101;i<199;i++) {
+			messages[i] = new GetRequest(keys[i - 101]);
+			shouldWait[i] = true;
+		}
+		/*
 		messages[3] = new PutRequest(key, value);	// should get SUCCESS back
-		shouldWait[0] = true;
-		messages[4] = messages[1];					// should get SUCCESS back
-		shouldWait[0] = true;
-		messages[5] = messages[1];					// should get KEY-DNE back
-		shouldWait[0] = true;
-		messages[6] = new ShutdownRequest();		// should get SUCCESS back
-		shouldWait[0] = false;
-		messages[7] = messages[0];					// should get nothing back
-		shouldWait[0] = true;
-		messages[8] = messages[0];
-		shouldWait[0] = true;
-		messages[9] = messages[0];
-		shouldWait[0] = true;
+		shouldWait[3] = true;
+		messages[4] = new PutRequest(key1,value);					// should get SUCCESS back
+		shouldWait[4] = true;
+		messages[5] = new PutRequest(key2,value);;					// should get KEY-DNE back
+		shouldWait[5] = true;
+		messages[6] = new PutRequest(key3,value);;		// should get SUCCESS back
+		shouldWait[6] = true;
+		messages[7] = new GetRequest(key);					// should get nothing back
+		shouldWait[7] = true;
+		messages[8] = new GetRequest(key1);
+		shouldWait[8] = true;
+		messages[9] = new GetRequest(key2);
+		shouldWait[9] = true;
+		messages[10] = new GetRequest(key3);
+		shouldWait[10] = true;*/
 		System.out.println("It has created the messages.");
 
 		// Send and receive messages
@@ -87,7 +103,7 @@ public class TestingClient {
 				System.out.print(messages[i].toString());
 				byte[] rcvData = new byte[15500];
 				receivePacket = new DatagramPacket(rcvData, rcvData.length);
-				if(!shouldWait[i]) { 
+				if(shouldWait[i]) { 
 					socket.receive(receivePacket);
 					System.out.println("- - Response:");
 					if (messages[i].command == GET && receivePacket.getData()[16] == 0) {
