fa1273c0641102af590a04479e8d89c7b98977bc
g17-sb <redhot_bouthot@hotmail.com>
2015-04-18 12:15:02 -0700

    isDead written to take care of replication

--- kvStore/src/com/cam/eece411/Handlers/UpdateHandler.java
+++ kvStore/src/com/cam/eece411/Handlers/UpdateHandler.java
@@ -1,11 +1,17 @@
 package com.cam.eece411.Handlers;
 
+import java.nio.ByteBuffer;
 import java.util.logging.Logger;
 
+import com.cam.eece411.Server;
+import com.cam.eece411.Communication.Builder;
 import com.cam.eece411.Communication.Message;
+import com.cam.eece411.Communication.UDPSocket;
 import com.cam.eece411.Structures.DHT;
+import com.cam.eece411.Structures.KVS;
 import com.cam.eece411.Structures.Node;
 import com.cam.eece411.Utilities.Commands;
+import com.cam.eece411.Utilities.Utils;
 
 public class UpdateHandler implements Runnable {
 	private static final Logger log = Logger.getLogger(UpdateHandler.class.getName());
@@ -38,7 +44,26 @@ public class UpdateHandler implements Runnable {
 	}
 	
 	private void handleIS_DEAD() {
+		//we have been messaged saying that some node is dead
+		//need to remove that node from the table
+		//also, if removing that node makes responsible for its files, we need to replicate on our watchers.
+		if(DHT.getNextNodeOf(Server.me).nodeID == msg.getNodeID()) {
+			//if the dead node is our next node
+			Node myPreviousNode = DHT.getPrevNodeOf(Server.me);
+			for (ByteBuffer key : KVS.getKeys()) {
+				//for each key in the table
+                
+                //if this key was the responsibility of the dead node
+                if(DHT.findNodeFor(key.array()).nodeID == msg.getNodeID()){
+                    UDPSocket socket = new UDPSocket(Utils.REP_PORT);
+                    socket.send(Builder.replicatedPut(msg),myPreviousNode.addr, Utils.MAIN_PORT);
+                    // then we need to put it on our previous node for replication.
+                    
+                }
+            }
+		}
 		if (DHT.remove(msg.getNodeID()) != null) {
+			// after taking care of that, remove the node from our circle.
 			log.info("Node " + msg.getNodeID() + " at " + msg.getNodeAddress().getHostName() + " was removed from the local DHT.");
 		}
 	}
