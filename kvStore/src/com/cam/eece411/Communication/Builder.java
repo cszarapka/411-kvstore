fde841f20d8042b91283adec17e3b7c2c0d047eb
g17-sb <redhot_bouthot@hotmail.com>
2015-04-25 16:05:20 -0700

    fixed echo-return

--- kvStore/src/com/cam/eece411/Communication/Builder.java
+++ kvStore/src/com/cam/eece411/Communication/Builder.java
@@ -245,11 +245,23 @@ public final class Builder {
 		buffer[index++] = originPort[3];
 
 
-		// Add rest of app response
-		byte[] bytes = ap.buffer;
-		for (int i = uniqueID.length; i < bytes.length; i++) {
-			buffer[index++] = bytes[i];
+		// Add response code TODO should this always be success?
+		buffer[index++] = ap.responseCode;
 
+
+
+		// Add value if get command
+		if(msg.getAppCommand() == Commands.GET) {
+			// Add value length
+			int valueLength = ap.getValue().length;// Add the value length
+			buffer[index++] = Utils.intToByteArray(valueLength)[0];
+			buffer[index++] = Utils.intToByteArray(valueLength)[1];
+
+			byte[] value = ap.getValue();
+			log.info("Value is " + Utils.bytesToHexString(value));
+			for(int i = 0; i < ap.getValue().length; i++){
+				buffer[index++] = value[i];
+			}
 		}
 
 		return buffer;
