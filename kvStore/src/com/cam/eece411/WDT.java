623c6852e2d1ed41a93c6b220eb063c6c3b93be0
g17-sb <redhot_bouthot@hotmail.com>
2015-04-25 17:38:23 -0700

    fixing curly fries

--- kvStore/src/com/cam/eece411/WDT.java
+++ kvStore/src/com/cam/eece411/WDT.java
@@ -10,16 +10,17 @@ import com.cam.eece411.Structures.Node;
 import com.cam.eece411.Utilities.Utils;
 
 /**
- * The Watchdog Thread - simply broadcasts an IS-ALIVE
- * message every now and then.
+ * The Watchdog Thread - simply broadcasts an IS-ALIVE message every now and
+ * then.
+ * 
  * @author cam
  *
  */
 public class WDT implements Runnable {
 	private static final Logger log = Logger.getLogger(WDT.class.getName());
-	
+
 	private UDPSocket socket;
-	
+
 	public WDT(int port) {
 		socket = new UDPSocket(port);
 	}
@@ -28,37 +29,47 @@ public class WDT implements Runnable {
 		log.info("Watchdog Thread launched");
 		while (true) {
 			// Wait for a bit
-			try { Thread.sleep(Utils.WDT_TIMEOUT); }
-			catch (InterruptedException e) { log.log(Level.SEVERE, e.toString(), e); }
-			
+			try {
+				Thread.sleep(Utils.WDT_TIMEOUT);
+			} catch (InterruptedException e) {
+				log.log(Level.SEVERE, e.toString(), e);
+			}
+
 			// Broadcast an IsAlive message
-			socket.broadcast(Builder.isAlive(Server.me), DHT.broadcastList(), Utils.MAIN_PORT);
-			
-			
-			
-			
-			//get current time
+			socket.broadcast(Builder.isAlive(Server.me), DHT.broadcastList(),
+					Utils.MAIN_PORT);
+
+			// get current time
 			long currentTimestamp = System.currentTimeMillis() / 1000L;
-			//max difference allowed is 2.5*WDT_TIMEOUT / 1000 [seconds]
+			// max difference allowed is 2.5*WDT_TIMEOUT / 1000 [seconds]
 			int maxDiff = (Utils.WDT_TIMEOUT * 2500) / 1000;
-			
-			//iterate through each node
+
+			// iterate through each node
 			for (Node node : DHT.nodes()) {
-				//find difference between last time the node was updated and the current time
+				// find difference between last time the node was updated and
+				// the current time
 				long timestampDiff = currentTimestamp - node.timestamp;
-				
-				// TODO: if one of the nodes has a really old timestamp, ping him <-- needed?
-				
-				//any node with with a timestamp older than maxDiff is declared dead 
-				if(timestampDiff > maxDiff)
-					//remove node from local dht
+
+				// TODO: if one of the nodes has a really old timestamp, ping
+				// him <-- needed?
+
+				// any node with with a timestamp older than maxDiff is declared
+				// dead
+				if (timestampDiff > maxDiff) {
+					log.info("Watchdog thread: broadcasting to all that node "
+							+ node.nodeID + "is dead.");
+					log.info("Current stamp: " + currentTimestamp
+							+ "; node stamp: " + node.timestamp);
+
+					// remove node from local dht
 					DHT.remove(node.nodeID);
-					//broadcast an isDead message
-					socket.broadcast(Builder.isDead(node), DHT.broadcastList(), Utils.MAIN_PORT);
-		    }
-			
+					// broadcast an isDead message
+					socket.broadcast(Builder.isDead(node), DHT.broadcastList(),
+							Utils.MAIN_PORT);
+				}
+			}
+
 		}
 	}
-	
-	
+
 }
