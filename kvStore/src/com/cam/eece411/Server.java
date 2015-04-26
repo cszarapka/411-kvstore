2d2623bc56aafd5ddce5db8f04cd63b91beb4a2f
g17-sb <redhot_bouthot@hotmail.com>
2015-04-26 16:43:01 -0700

    new jar

--- kvStore/src/com/cam/eece411/Server.java
+++ kvStore/src/com/cam/eece411/Server.java
@@ -44,7 +44,7 @@ public class Server {
 
 	public static void main(String[] args) throws SocketException, IOException, InterruptedException {
 		
-		log.info("And so it begins. (V3)");
+		log.info("And so it begins. (V4)");
 
 		// Instantiate ourself as a node and set our state
 		setup();
