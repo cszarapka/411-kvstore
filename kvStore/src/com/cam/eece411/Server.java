19d56a96d513a08c81c218c730619f20191fb434
g17-sb <redhot_bouthot@hotmail.com>
2015-04-27 06:34:35 -0700

    version 24

--- kvStore/src/com/cam/eece411/Server.java
+++ kvStore/src/com/cam/eece411/Server.java
@@ -45,7 +45,7 @@ public class Server {
 
 	public static void main(String[] args) throws SocketException, IOException, InterruptedException {
 		
-		log.info("And so it begins. (V23)");
+		log.info("And so it begins. (V24)");
 
 		// Instantiate ourself as a node and set our state
 		setup();
